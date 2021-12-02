f0e24e749882f3903eaf497f9ed70a66f47a97d6
96s.e <85901055+EgorSavchuk@users.noreply.github.com>
2021-12-03 01:51:21 +0500

    Update CreateDate.py

--- CreateDate.py
+++ CreateDate.py
@@ -1,13 +1,13 @@
 import random
 from datetime import date
-
-start_date = date.today().replace(day=1, month=1).toordinal()
-end_date = date.today().toordinal()
+start_date = date.today().replace(day=1, month=1).toordinal()  # Дата от которой будет выбираться случайная дата
+end_date = date.today().toordinal()  # Дата до которой будет выбраться случайная дата
 f = open('date.txt', 'w')
-n = 50
-line: str = "('13', '10', '1', '7', '2021-12-08'),"
-for i in range(15, 51):
-    if i == 50:
+start = 15  # От какого id
+end = 51  # До какого id
+line: str = ""
+for i in range(start, end):
+    if i == end - 1:
         line += f"\n('{i}', '{random.randint(1, 10)}', '{random.randint(1, 10)}', '{random.randint(1, 10)}', " \
                 f"'{date.fromordinal(random.randint(start_date, end_date))}'); "
     else:
