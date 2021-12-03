07761dc1d8a58d7d93901ab8e5346b60263495ca
96s.e <85901055+EgorSavchuk@users.noreply.github.com>
2021-12-03 16:08:52 +0000

    Add files via upload

---
+++ CreateData.py
@@ -0,0 +1,52 @@
+import random
+from datetime import date
+
+
+def generate_date():  # Генерация случайной даты
+    start_date = date.today().replace(day=1, month=1).toordinal()  # Дата от которой будет выбираться случайная дата
+    end_date = date.today().toordinal()  # Дата до которой будет выбраться случайная дата
+    return date.fromordinal(random.randint(start_date, end_date))
+
+
+def generate_fullname():  # Генерация случайного ФИО
+    from lib import FullNames
+    return FullNames[random.randint(1, 100)]
+
+
+def generate_email(to_full_name: str = ''):  # Генерация случайного email, если в функцию передать ФИО в формате -
+    # Фамилия Имя Отчество вернет email в формате вышкинской почты
+    if to_full_name == '':
+        return f'example{random.randint(1,100)}@miem.hse.ru'
+    else:
+        from lib import Dict
+        fio_array = to_full_name.split(' ')
+        first = Dict.get(fio_array[1][0])
+        second = Dict.get(fio_array[2][0])
+        email = f'{first}{second}'
+        for j in fio_array[0]:
+            email += Dict.get(j)
+        email += '@miem.hse.ru'
+        return email
+
+
+def generate_text(symbols: int = 20):  # Генерирует случайный текст длинной symbols, по умолчанию 20 символов
+    from lib import Text
+    return Text[:symbols]
+
+
+#  Пример использования, Допустим нужны строки (id, name, email, data , text)
+data = open('data.txt', 'w')
+start = 1  # От какого id
+end = 51  # До какого id
+result: str = ""  # Итоговая строка
+for i in range(start, end):
+    # Генерация имени, чтобы от него же можно было сгенерировать почту
+    fullname = generate_fullname()
+    # Строка в формате SQL VALUES ('значение' , 'значение2')
+    line = f"('{i}', '{fullname}', '{generate_email(fullname)}', '{generate_date()}', '{generate_text(35)}')"
+    if i == end - 1:  # Добавляет ; в конце, для последней строчки
+        result += f"{line};"
+    else:
+        result += f"{line},\n"  # Добавляет переход строки для всех строчек, кроме последней
+data.write(result)
+
---
+++ lib.py
@@ -0,0 +1,155 @@
+FullNames = [
+    'Короткова Фатима Тимофеевна',
+    'Яковлева Ольга Ярославовна',
+    'Иванов Лев Даниэльевич',
+    'Шишкин Ян Георгиевич',
+    'Романов Лев Артёмович',
+    'Устинов Александр Леонидович',
+    'Панкова Амелия Александровна',
+    'Овчинников Лев Георгиевич',
+    'Борисова Екатерина Адамовна',
+    'Лазарева Вероника Фёдоровна',
+    'Сергеева Алина Павловна',
+    'Терехова Ева Артёмовна',
+    'Бессонова Валерия Егоровна',
+    'Кузнецов Мирон Фёдорович',
+    'Федоров Марк Львович',
+    'Игнатьев Роман Мирославович',
+    'Степанова Мелания Юрьевна',
+    'Чернов Даниил Петрович',
+    'Филатова Валерия Данииловна',
+    'Смирнов Владимир Львович',
+    'Рудаков Матвей Фёдорович',
+    'Матвеев Александр Вячеславович',
+    'Дубровин Дмитрий Тимофеевич',
+    'Котова Ксения Александровна',
+    'Смирнова Мария Макаровна',
+    'Егоров Фёдор Артёмович',
+    'Михайлов Эмир Максимович',
+    'Зуева Арина Максимовна',
+    'Калмыков Илья Тимофеевич',
+    'Белякова Анна Антоновна',
+    'Королев Юрий Робертович',
+    'Степанова Ольга Петровна',
+    'Дружинин Константин Викторович',
+    'Алексеева Алиса Георгиевна',
+    'Волошин Сергей Егорович',
+    'Журавлева Анастасия Макаровна',
+    'Осипова Яна Романовна',
+    'Столяров Александр Григорьевич',
+    'Ерофеев Ярослав Даниилович',
+    'Чумаков Иван Максимович',
+    'Сергеева Алиса Егоровна',
+    'Петровская Анастасия Тимофеевна',
+    'Галкин Денис Михайлович',
+    'Еремин Николай Артемьевич',
+    'Орехова Ясмина Фёдоровна',
+    'Кузнецов Фёдор Владимирович',
+    'Судаков Дмитрий Артёмович',
+    'Беляков Алексей Ильич',
+    'Максимов Владимир Николаевич',
+    'Семенов Михаил Ярославович',
+    'Молчанова Александра Леоновна',
+    'Демин Никита Артёмович',
+    'Селиванов Максим Викторович',
+    'Малахова Эмилия Тихоновна',
+    'Иванова Ульяна Георгиевна',
+    'Дорохов Глеб Васильевич',
+    'Новиков Макар Даниилович',
+    'Сазонов Кирилл Максимович',
+    'Лебедева Ульяна Арсентьевна',
+    'Туманова Вера Львовна',
+    'Кузьмин Максим Владимирович',
+    'Родионов Максим Георгиевич',
+    'Агеев Ярослав Елисеевич',
+    'Иванова Евгения Кирилловна',
+    'Смирнов Степан Артёмович',
+    'Федосеев Александр Родионович',
+    'Грачев Вячеслав Ибрагимович',
+    'Григорьева Анастасия Константиновна',
+    'Демин Степан Романович',
+    'Васильева Екатерина Дмитриевна',
+    'Голиков Николай Тимурович',
+    'Чижов Артём Саввич',
+    'Агафонов Аркадий Ярославович',
+    'Шапошников Олег Александрович',
+    'Никифорова Анна Евгеньевна',
+    'Чернов Владимир Львович',
+    'Демидова Анастасия Андреевна',
+    'Носков Максим Кириллович',
+    'Никифорова Анна Максимовна',
+    'Баранов Дмитрий Ярославович',
+    'Пахомова Дарина Кирилловна',
+    'Малышев Даниил Германович',
+    'Кошелев Алексей Артёмович',
+    'Горячева Алиса Даниэльевна',
+    'Владимиров Максим Ильич',
+    'Миронов Степан Александрович',
+    'Романова Ольга Сергеевна',
+    'Сальникова Екатерина Фёдоровна',
+    'Ильина Элина Максимовна',
+    'Семенова Анна Ивановна',
+    'Сахаров Захар Сергеевич',
+    'Грачев Фёдор Олегович',
+    'Киселев Иван Максимович',
+    'Попов Фёдор Антонович',
+    'Рыбаков Константин Макарович',
+    'Токарева Надежда Ивановна',
+    'Зайцева Валерия Олеговна',
+    'Самойлов Михаил Львович',
+    'Кочергин Александр Ярославович',
+    'Овчинников Савва Михайлович'
+]
+Dict = {'Ь': '', 'ь': '', 'Ъ': '', 'ъ': '', 'А': 'a', 'а': 'a', 'Б': 'b', 'б': 'b', 'В': 'v', 'в': 'v',
+        'Г': 'g', 'г': 'g', 'Д': 'd', 'д': 'd', 'Е': 'e', 'е': 'e', 'Ё': 'e', 'ё': 'e', 'Ж': 'zh', 'ж': 'zh',
+        'З': 'z', 'з': 'z', 'И': 'i', 'и': 'i', 'Й': 'i', 'й': 'i', 'К': 'k', 'к': 'k', 'Л': 'l', 'л': 'l',
+        'М': 'm', 'м': 'm', 'Н': 'n', 'н': 'n', 'О': 'o', 'о': 'o', 'П': 'p', 'п': 'p', 'Р': 'r', 'р': 'r',
+        'С': 's', 'с': 's', 'Т': 't', 'т': 't', 'У': 'u', 'у': 'u', 'Ф': 'f', 'ф': 'f', 'Х': 'kh', 'х': 'kh',
+        'Ц': 'tc', 'ц': 'tc', 'Ч': 'ch', 'ч': 'ch', 'Ш': 'sh', 'ш': 'sh', 'Щ': 'shch', 'щ': 'shch', 'Ы': 'y',
+        'ы': 'y', 'Э': 'e', 'э': 'e', 'Ю': 'iu', 'ю': 'iu', 'Я': 'ia', 'я': 'ia', ' ': ''}
+Text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+dolore magna aliqua. Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Sed libero enim sed faucibus 
+turpis in eu mi bibendum. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Risus pretium quam 
+vulputate dignissim. Ullamcorper sit amet risus nullam eget felis eget nunc lobortis. Facilisi nullam vehicula ipsum 
+a. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Pellentesque habitant morbi tristique senectus 
+et netus et malesuada. Consequat ac felis donec et odio pellentesque. Eget est lorem ipsum dolor sit amet consectetur 
+adipiscing elit. Est placerat in egestas erat imperdiet sed euismod nisi porta. Laoreet suspendisse interdum 
+consectetur libero id faucibus nisl. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Tellus in 
+metus vulputate eu scelerisque felis imperdiet proin. Cras sed felis eget velit aliquet sagittis id consectetur. 
+Phasellus vestibulum lorem sed risus ultricies. Vel eros donec ac odio tempor orci. Justo nec ultrices dui sapien 
+eget mi proin. Ipsum nunc aliquet bibendum enim facilisis gravida. Semper risus in hendrerit gravida. Amet volutpat 
+consequat mauris nunc congue nisi vitae suscipit tellus. Maecenas accumsan lacus vel facilisis volutpat est velit 
+egestas. Urna porttitor rhoncus dolor purus non enim praesent. Id diam vel quam elementum pulvinar etiam non. Lacus 
+vestibulum sed arcu non odio. Nullam vehicula ipsum a arcu cursus vitae. Scelerisque varius morbi enim nunc faucibus 
+a pellentesque sit amet. Quam viverra orci sagittis eu volutpat odio facilisis. Aenean euismod elementum nisi quis 
+eleifend. Etiam sit amet nisl purus in mollis. Volutpat sed cras ornare arcu dui. Tincidunt arcu non sodales neque 
+sodales ut etiam sit. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Sed velit dignissim sodales 
+ut eu sem. Nulla aliquet enim tortor at auctor urna nunc. Consequat ac felis donec et. Urna nunc id cursus metus 
+aliquam eleifend. Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Tellus elementum sagittis vitae 
+et leo duis. Orci porta non pulvinar neque. Sit amet mattis vulputate enim nulla aliquet porttitor lacus. Neque 
+viverra justo nec ultrices dui sapien eget mi. Aenean euismod elementum nisi quis eleifend. Mauris commodo quis 
+imperdiet massa tincidunt nunc pulvinar sapien et. Dignissim convallis aenean et tortor at. Pretium viverra 
+suspendisse potenti nullam. Tristique et egestas quis ipsum. Ut consequat semper viverra nam libero justo laoreet sit 
+amet. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Convallis aenean et tortor at risus viverra. 
+Pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus et. Et sollicitudin ac orci phasellus 
+egestas tellus rutrum tellus. A iaculis at erat pellentesque adipiscing commodo elit at. Donec adipiscing tristique 
+risus nec feugiat in fermentum posuere. Euismod elementum nisi quis eleifend quam adipiscing. Sed viverra tellus in 
+hac habitasse. At lectus urna duis convallis convallis tellus id interdum velit. Aliquam faucibus purus in massa 
+tempor nec feugiat. Tellus mauris a diam maecenas sed enim ut sem viverra. Sed augue lacus viverra vitae congue eu 
+consequat. Pellentesque pulvinar pellentesque habitant morbi tristique. Est ullamcorper eget nulla facilisi etiam 
+dignissim. Orci dapibus ultrices in iaculis nunc. Convallis posuere morbi leo urna molestie at. Tellus integer 
+feugiat scelerisque varius morbi enim nunc. Augue eget arcu dictum varius duis at. Ligula ullamcorper malesuada proin 
+libero nunc. In cursus turpis massa tincidunt dui ut ornare lectus sit. Dictum at tempor commodo ullamcorper a lacus. 
+In massa tempor nec feugiat nisl. Nunc non blandit massa enim nec dui nunc mattis. Fames ac turpis egestas maecenas 
+pharetra. Id ornare arcu odio ut sem nulla pharetra diam sit. Massa massa ultricies mi quis hendrerit. Sed libero 
+enim sed faucibus turpis in eu mi bibendum. Arcu dui vivamus arcu felis. At quis risus sed vulputate odio ut enim. 
+Fringilla urna porttitor rhoncus dolor purus non. Sit amet est placerat in egestas erat imperdiet. Quam vulputate 
+dignissim suspendisse in est ante. Tincidunt augue interdum velit euismod in pellentesque massa placerat. Suscipit 
+tellus mauris a diam maecenas sed. Maecenas ultricies mi eget mauris pharetra et ultrices neque ornare. Lorem ipsum 
+dolor sit amet consectetur adipiscing elit ut aliquam. Pellentesque massa placerat duis ultricies lacus sed turpis 
+tincidunt. In cursus turpis massa tincidunt dui. In tellus integer feugiat scelerisque varius. Cras tincidunt 
+lobortis feugiat vivamus. Cras pulvinar mattis nunc sed. Nisl rhoncus mattis rhoncus urna. Urna id volutpat lacus 
+laoreet non curabitur gravida. Amet nisl purus in mollis nunc sed. Sagittis nisl rhoncus mattis rhoncus urna neque. 
+Cras sed felis eget velit aliquet sagittis id consectetur purus. Sed faucibus turpis in eu mi. Dignissim enim sit 
+amet venenatis urna cursus eget nunc. Est placerat in egestas erat imperdiet sed euismod nisi. Montes nascetur 
+ridiculus mus mauris. '''
